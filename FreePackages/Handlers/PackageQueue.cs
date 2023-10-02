2a8787480b1cb8c8583cef31cb72981153144d7d
Citrinate <phil4387@gmail.com>
2023-10-02 09:35:45 -0400

    Fix false "Rate Limited" error

--- FreePackages/Handlers/PackageQueue.cs
+++ FreePackages/Handlers/PackageQueue.cs
@@ -169,10 +169,12 @@ namespace FreePackages {
 					return EResult.OK;
 				}
 
-				// Assume we're rate limited
-				Bot.ArchiLogger.LogGenericInfo(string.Format("ID: app/{0} | Status: {1}", appID, EResult.RateLimitExceeded));
+				// We could be rate limited, but the app could also be invalid beacause it has no available licenses.  It's necessary to assume invalid so we don't get into an infinite loop.
+				// Examples: https://steamdb.info/app/2401570/ on Oct 2, 2023, Attempting to download demo through Steam client gives error "no licenses"
+				// Free games that still have store pages but display "At the request of the publisher, ___ is unlisted on the Steam store and will not appear in search.": https://store.steampowered.com/app/376570/WildStar/
+				Bot.ArchiLogger.LogGenericInfo(string.Format("ID: app/{0} | Status: {1}", appID, EResult.Invalid));
 
-				return EResult.RateLimitExceeded;
+				return EResult.Invalid;
 			}
 
 			return EResult.OK;
