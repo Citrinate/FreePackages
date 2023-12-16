1fe9db0045ba571efb2e4b49be6deadda9c39cbe
Citrinate <phil4387@gmail.com>
2023-12-15 19:04:32 -0500

    Added tests

--- FreePackages.Tests/Apps.cs
+++ FreePackages.Tests/Apps.cs
@@ -25,31 +25,31 @@ public class Apps {
 		PackageFilter.Country = "FOO";
 	}
 
-    [TestMethod]
-    public void CanDetectFreeApp() {
+	[TestMethod]
+	public void CanDetectFreeApp() {
 		var app = new FilterableApp(KeyValue.LoadAsText("app_which_is_free.txt"));
 
 		Assert.IsTrue(app.IsFree());
-    }
+	}
 
-    [TestMethod]
-    public void CanDetectAvailableAppByReleaseState() {
+	[TestMethod]
+	public void CanDetectAvailableAppByReleaseState() {
 		var app = new FilterableApp(KeyValue.LoadAsText("app_with_release_state.txt"));
 
 		Assert.IsFalse(app.ReleaseState.IsNullOrEmpty());
 		Assert.IsTrue(app.IsAvailable());
-    }
+	}
 
-    [TestMethod]
-    public void CanDetectAvailableAppByState() {
+	[TestMethod]
+	public void CanDetectAvailableAppByState() {
 		var app = new FilterableApp(KeyValue.LoadAsText("app_with_state.txt"));
 
 		Assert.IsFalse(app.State.IsNullOrEmpty());
 		Assert.IsTrue(app.IsAvailable());
-    }
+	}
 
-    [TestMethod]
-    public void CanDetectRedeemableAppWithAppRequirement() {
+	[TestMethod]
+	public void CanDetectRedeemableAppWithAppRequirement() {
 		var app = new FilterableApp(KeyValue.LoadAsText("app_with_required_app.txt"));
 		var userData = JsonConvert.DeserializeObject<UserData>(File.ReadAllText("userdata_empty.json"));
 		userData.OwnedApps.Add(1086940);
@@ -57,10 +57,10 @@ public class Apps {
 
 		Assert.IsTrue(app.MustOwnAppToPurchase > 0);
 		Assert.IsTrue(PackageFilter.IsRedeemableApp(app));
-    }
+	}
 
-    [TestMethod]
-    public void CanDetectRedeemableAppWithRestrictedCountry() {
+	[TestMethod]
+	public void CanDetectRedeemableAppWithRestrictedCountry() {
 		var app = new FilterableApp(KeyValue.LoadAsText("app_with_restricted_countries.txt"));
 
 		Assert.IsTrue(app.RestrictedCountries.Contains("DE"));
@@ -69,10 +69,10 @@ public class Apps {
 		PackageFilter.Country = "dE";
 
 		Assert.IsFalse(PackageFilter.IsRedeemableApp(app));
-    }
+	}
 
-    [TestMethod]
-    public void CanDetectRedeemableAppWithPurchaseRestrictedCountry() {
+	[TestMethod]
+	public void CanDetectRedeemableAppWithPurchaseRestrictedCountry() {
 		var app = new FilterableApp(KeyValue.LoadAsText("app_with_purchase_restricted_countries.txt"));
 
 		Assert.IsTrue(app.AllowPurchaseFromRestrictedCountries);
@@ -82,23 +82,23 @@ public class Apps {
 		PackageFilter.Country = "uS";
 
 		Assert.IsTrue(PackageFilter.IsRedeemableApp(app));
-    }
+	}
 
-    [TestMethod]
-    public void CanFindAppDLC() {
+	[TestMethod]
+	public void CanFindAppDLC() {
 		var app = new FilterableApp(KeyValue.LoadAsText("app_with_dlc.txt"));
 		PackageFilter.Country = "";
 
 		Assert.IsFalse(app.ListOfDLC.IsNullOrEmpty());
-    }
+	}
 
-    [TestMethod]
-    public void CanDetectNonRedeemablePlaytestWithHiddenParent() {
+	[TestMethod]
+	public void CanDetectNonRedeemablePlaytestWithHiddenParent() {
 		var playtest = new FilterableApp(KeyValue.LoadAsText("playtest_with_hidden_parent.txt"));
 		var playtestParent = KeyValue.LoadAsText("playtest_with_hidden_parent_parent.txt");
 		playtest.AddParent(playtestParent);
 
 		Assert.IsTrue(playtest.Parent.Hidden);
 		Assert.IsFalse(PackageFilter.IsRedeemablePlaytest(playtest));
-    }
+	}
 }
