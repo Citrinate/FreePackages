84b36fcc7815a16a3a41a62ba6923b15b8d26d88
Citrinate <phil4387@gmail.com>
2025-06-02 16:03:21 -0400

    FreePackagesImporter V1.0.3
    - Move IPC password setting to userscript manager popup menu
    - Fix styling

--- FreePackagesImporter/code.user.js
+++ FreePackagesImporter/code.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/Citrinate
 // @author      Citrinate
 // @description Transfer packages from SteamDB's free packages tool to the ASF Free Packages plugin
-// @version     1.0.2
+// @version     1.0.3
 // @match       *://steamdb.info/freepackages/*
 // @connect     localhost
 // @connect     127.0.0.1
@@ -11,6 +11,7 @@
 // @grant       GM_xmlhttpRequest
 // @grant       GM_getValue
 // @grant       GM_setValue
+// @grant       GM_registerMenuCommand
 // @homepageURL https://github.com/Citrinate/FreePackages
 // @supportURL  https://github.com/Citrinate/FreePackages/issues
 // @downloadURL https://raw.githubusercontent.com/Citrinate/FreePackages/main/FreePackagesImporter/code.user.js
@@ -40,6 +41,15 @@
 	function SetSetting(name, value) {
 		GM_setValue(name, value);
 	}
+
+	GM_registerMenuCommand("Set ASF IPC Password", () => {
+		const password = prompt("Enter ASF IPC Password", GetSetting(SETTING_ASF_PASSWORD));
+
+		if (password !== null) {
+			SetSetting(SETTING_ASF_PASSWORD, password);
+			window.location.reload();
+		}
+	});
 	//#endregion
 
 	BuildInterface();
@@ -145,7 +155,7 @@
 	function BuildInterface() {
 		document.getElementById("freepackages").insertAdjacentHTML("afterend", `
 			<div class="panel" style="border-color: #669900;">
-				<div class="panel-heading d-flex" style="background-color: #436600; align-items: center;">
+				<div class="panel-heading d-flex" style="background-color: #436600; background-image: none; align-items: center;">
 					<span class="flex-grow">Add using Free Packages Plugin</span>
 					<button id="js-freepackages-settings-button" class="btn btn-sm" style="background-color: #b86795; color: #ddd; border-color: #90426f;"><svg width="16" height="16" viewBox="0 0 924.001 924.001" class="octicon" aria-hidden="true"><path d="M841.36,187.993L736.009,82.64c-6.51-6.51-16.622-7.735-24.499-2.968l-54.938,33.252 c-26.704-14.917-55.296-26.858-85.328-35.375l-15.326-62.326C553.719,6.284,545.702,0,536.496,0h-148.99 c-9.206,0-17.223,6.284-19.421,15.224L352.759,77.55c-30.032,8.517-58.624,20.458-85.328,35.375l-54.938-33.252 c-7.876-4.767-17.989-3.542-24.499,2.968L82.642,187.993c-6.51,6.51-7.735,16.622-2.968,24.498l33.252,54.938 c-14.917,26.704-26.857,55.296-35.375,85.328l-62.326,15.326c-8.94,2.199-15.224,10.216-15.224,19.422v148.99 c0,9.206,6.284,17.223,15.224,19.421l62.326,15.326c8.517,30.032,20.458,58.624,35.375,85.328l-33.252,54.938 c-4.767,7.876-3.542,17.988,2.968,24.498L187.993,841.36c6.51,6.509,16.622,7.734,24.499,2.968l54.938-33.252 c26.704,14.917,55.295,26.856,85.328,35.375l15.326,62.326c2.198,8.939,10.215,15.224,19.421,15.224h148.99 c9.206,0,17.223-6.284,19.421-15.224l15.326-62.326c30.032-8.518,58.624-20.458,85.328-35.375l54.938,33.252 c7.876,4.767,17.989,3.542,24.499-2.968l105.353-105.353c6.51-6.51,7.734-16.622,2.968-24.498l-33.252-54.938 c14.917-26.704,26.856-55.296,35.375-85.328l62.326-15.326C917.716,553.72,924,545.703,924,536.497v-148.99 c0-9.206-6.284-17.223-15.224-19.421L846.45,352.76c-8.518-30.032-20.458-58.624-35.375-85.328l33.252-54.938 C849.095,204.615,847.87,194.502,841.36,187.993z M462.001,670.481c-115.141,0-208.48-93.341-208.48-208.481 c0-115.141,93.34-208.481,208.48-208.481S670.482,346.859,670.482,462C670.482,577.14,577.142,670.481,462.001,670.481z"></path></svg> Settings</button>
 				</div>
@@ -180,8 +190,11 @@
 						<dt class="span2">
 							<label for="js-freepackages-settings-asf-password">ASF IPC Password</label>
 						</dt>
-						<dd>
-							<input type="text" id="js-freepackages-settings-asf-password" placeholder="${defaultSettings[SETTING_ASF_PASSWORD]}" value="${GetSetting(SETTING_ASF_PASSWORD)}"></input>
+						<dd style="color:var(--muted-color)">
+							<div style="color:var(--muted-color)">
+								<svg width="16" height="16" viewBox="0 0 16 16" class="octicon octicon-info" aria-hidden="true"><path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8Zm8-6.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13ZM6.5 7.75A.75.75 0 0 1 7.25 7h1a.75.75 0 0 1 .75.75v2.75h.25a.75.75 0 0 1 0 1.5h-2a.75.75 0 0 1 0-1.5h.25v-2h-.25a.75.75 0 0 1-.75-.75ZM8 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"></path></svg>
+								This setting can be configured from your userscript manager's popup menu, found in your browser's extensions toolbar
+							</div>
 						</dd>
 					</dl>
 					<dl class="form flattened">
