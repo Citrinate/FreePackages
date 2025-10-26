29a4cf47685781a864e135ac8138dd6d7deb0e52
Łukasz Domeradzki <JustArchi@JustArchi.net>
2025-10-26 14:49:20 +0100

    Improve packages tests

--- FreePackages.Tests/Packages.cs
+++ FreePackages.Tests/Packages.cs
@@ -1,72 +1,141 @@
-using System.Collections.Generic;
+using System;
 using System.IO;
+using System.Threading.Tasks;
 using ArchiSteamFarm.Helpers.Json;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SteamKit2;
 
 namespace FreePackages.Tests;
 
-[TestClass]
 [DeploymentItem("TestData")]
-public class Packages {
-	internal PackageFilter PackageFilter;
+[TestClass]
+public sealed class Packages : IDisposable {
+	private BotCache? BotCache;
+	private PackageFilter? PackageFilter;
 
 	[TestInitialize]
-	public void InitializePackageFilter () {
-		PackageFilter = new PackageFilter(new BotCache(), new List<FilterConfig>());
-		PackageFilter.UpdateUserDetails(File.ReadAllText("userdata_empty.json").ToJsonObject<Steam.UserData>(), File.ReadAllText("userinfo_empty.json").ToJsonObject<Steam.UserInfo>());
+	public async Task InitializePackageFilter () {
+		Dispose();
+
+		BotCache = new BotCache();
+		PackageFilter = new PackageFilter(BotCache, []);
+
+		(Steam.UserData userData, Steam.UserInfo userInfo) = await GetEmptyDetails().ConfigureAwait(false);
+
+		PackageFilter.UpdateUserDetails(userData, userInfo);
 		PackageFilter.Country = "FOO";
 	}
 
 	[TestCleanup]
-	public void CleanupPackageFilter() {
-		PackageFilter.UpdateUserDetails(File.ReadAllText("userdata_empty.json").ToJsonObject<Steam.UserData>(), File.ReadAllText("userinfo_empty.json").ToJsonObject<Steam.UserInfo>());
+	public async Task CleanupPackageFilter() {
+		if (PackageFilter == null) {
+			throw new InvalidOperationException(nameof(PackageFilter));
+		}
+
+		(Steam.UserData userData, Steam.UserInfo userInfo) = await GetEmptyDetails().ConfigureAwait(false);
+
+		PackageFilter.UpdateUserDetails(userData, userInfo);
 		PackageFilter.Country = "FOO";
 	}
 
     [TestMethod]
     public void CanDetectFreePackage() {
-		var package = new FilterablePackage(KeyValue.LoadAsText("package_which_is_free.txt"));
+		KeyValue? kv = KeyValue.LoadAsText("package_which_is_free.txt");
+
+		if (kv == null) {
+			throw new InvalidOperationException(nameof(kv));
+		}
+
+		FilterablePackage package = new(kv);
 
 		Assert.IsTrue(package.IsFree());
     }
 
-
     [TestMethod]
     public void CanDetectPackageDemoState() {
-		var package = new FilterablePackage(KeyValue.LoadAsText("package_with_deactivated_demo.txt"));
+	    KeyValue? kv = KeyValue.LoadAsText("package_with_deactivated_demo.txt");
+
+	    if (kv == null) {
+		    throw new InvalidOperationException(nameof(kv));
+	    }
+
+	    FilterablePackage package = new(kv);
 
 		Assert.IsTrue(package.DeactivatedDemo);
     }
 
     [TestMethod]
     public void CanDetectPackageTimeRestrictions() {
-		var package = new FilterablePackage(KeyValue.LoadAsText("package_with_timed_activation.txt"));
+	    KeyValue? kv = KeyValue.LoadAsText("package_with_timed_activation.txt");
 
-		Assert.IsTrue(package.ExpiryTime > 0);
-		Assert.IsTrue(package.StartTime > 0);
+	    if (kv == null) {
+		    throw new InvalidOperationException(nameof(kv));
+	    }
+
+	    FilterablePackage package = new(kv);
+
+		Assert.IsGreaterThan<ulong>(0, package.ExpiryTime);
+		Assert.IsGreaterThan<ulong>(0, package.StartTime);
     }
 
     [TestMethod]
     public void CanDetectPackageDisallowedApp() {
-		var package = new FilterablePackage(KeyValue.LoadAsText("package_with_disallowed_app.txt"));
+	    KeyValue? kv = KeyValue.LoadAsText("package_with_disallowed_app.txt");
 
-		Assert.IsTrue(package.DontGrantIfAppIDOwned > 0);
+	    if (kv == null) {
+		    throw new InvalidOperationException(nameof(kv));
+	    }
+
+	    FilterablePackage package = new(kv);
+
+		Assert.IsGreaterThan<uint>(0, package.DontGrantIfAppIDOwned);
     }
 
     [TestMethod]
     public void CanDetectPackageRestrictedCountry() {
-		var package = new FilterablePackage(KeyValue.LoadAsText("package_with_restricted_countries.txt"));
+	    KeyValue? kv = KeyValue.LoadAsText("package_with_restricted_countries.txt");
+
+	    if (kv == null) {
+		    throw new InvalidOperationException(nameof(kv));
+	    }
+
+	    FilterablePackage package = new(kv);
 
 		Assert.IsTrue(package.OnlyAllowRestrictedCountries);
-		Assert.IsTrue(package.RestrictedCountries.Contains("DE"));
+		Assert.IsNotNull(package.RestrictedCountries);
+		Assert.Contains("DE", package.RestrictedCountries);
     }
 
     [TestMethod]
     public void CanDetectPackagePurchaseRestrictedCountry() {
-		var package = new FilterablePackage(KeyValue.LoadAsText("package_with_purchase_restricted_countries.txt"));
+	    KeyValue? kv = KeyValue.LoadAsText("package_with_purchase_restricted_countries.txt");
+
+	    if (kv == null) {
+		    throw new InvalidOperationException(nameof(kv));
+	    }
+
+		FilterablePackage package = new(kv);
 
 		Assert.IsTrue(package.AllowPurchaseFromRestrictedCountries);
-		Assert.IsTrue(package.PurchaseRestrictedCountries.Contains("US"));
+		Assert.IsNotNull(package.PurchaseRestrictedCountries);
+		Assert.Contains("US", package.PurchaseRestrictedCountries);
     }
+
+	public void Dispose() => BotCache?.Dispose();
+
+	private static async Task<(Steam.UserData UserData, Steam.UserInfo UserInfo)> GetEmptyDetails() {
+		Steam.UserData? userData = (await File.ReadAllTextAsync("userdata_empty.json").ConfigureAwait(false)).ToJsonObject<Steam.UserData>();
+
+		if (userData == null) {
+			throw new InvalidOperationException(nameof(userData));
+		}
+
+		Steam.UserInfo? userInfo = (await File.ReadAllTextAsync("userinfo_empty.json").ConfigureAwait(false)).ToJsonObject<Steam.UserInfo>();
+
+		if (userInfo == null) {
+			throw new InvalidOperationException(nameof(userData));
+		}
+
+		return (userData, userInfo);
+	}
 }
